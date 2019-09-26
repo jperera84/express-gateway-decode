ba79cf78f2a5276d149dd17c26d1a1d502bb366e
jperera84 <jpereramor@gmail.com>
2019-09-26 11:08:23 -0400

    updated decoded

--- policies/decode-auth.js
+++ policies/decode-auth.js
@@ -7,7 +7,7 @@ module.exports = {
         const tokenArray = tokenHeader.split(' ');
         const tokenCifer = tokenArray[1];
         const bytes  = cryptojs.AES.decrypt(tokenCifer, '5F33468BECE4BFBCCACF4F2A9C112');
-        var token = bytes.toString(cryptojs.enc.Utf8);
+        var token = bytes.toString();
         req.headers.authorization = `Bearer ${token}`;
         next();
       };
